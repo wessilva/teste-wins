0b01ada0c0b4ed7aab4eb6a02b9158f68294250f
wessilva <silveirawesley.1996@gmail.com>
2025-09-18 20:53:36 -0300

    feat: Hero Section

--- src/components/HeroSection/FireArmsCard.tsx
+++ src/components/HeroSection/FireArmsCard.tsx
@@ -1,30 +1,21 @@
+interface FireArmsCardProps {
+    name: string;
+    price: string | number;
+    images: string;
+}
 
-
-export function FireArmsCard() {
-   
-
+export function FireArmsCard({ name, price, images }: FireArmsCardProps) {
     return (
-        <div className="w-full ">
-            <ul className="flex justify-center gap-2 items-center ">
-                <li className=" h-[198px] w-[178px]">
-                    <img src="/PurpleCard.png" alt="AK47" className="w-full h-full "/>
-                </li>
-                <li className=" h-[198px] w-[178px]">
-                    <img src="/PurpleCard.png" alt="AK47" className="w-full h-full " />
-                </li>
-                <li className=" h-[198px] w-[178px]">
-                    <img src="/PurpleCard.png" alt="AK47" className="w-full h-full" />
-                </li>
-                <li className=" h-[198px] w-[178px]">
-                    <img src="/PurpleCard.png" alt="AK47" className="w-full h-full " />
-                </li>
-                <li className=" h-[198px] w-[178px]">
-                    <img src="/PurpleCard.png" alt="AK47" className="w-full h-full " />
-                </li>
-               
-
-           
-            </ul>
-        </div>
+        <ul className="flex justify-center gap-2 items-center ">
+            <li className="flex flex-col h-[198px] w-[178px] justify-center items-center ">
+                <img src="/PurpleCard.png" alt={name} className="h-[200px] w-[180px]   " />
+                <img src={images} alt={name} className="max-h-[100px] max-w-[150px] absolute mb-14 object-cover" />
+                <h1 className="absolute mt-20">{name}</h1>
+                <div className="flex absolute items-center justify-center mt-28">
+                    <img src="/bgPrice.svg" className="h-16 w-30 inline-block mt-5" alt="Preço" />
+                    <span className="absolute mt-5 text-white font-bold text-[14px]">{price} QNT</span>
+                </div>
+            </li>
+        </ul>
     );
 }
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,8 +1,15 @@
 import { FireArmsCard } from "./FireArmsCard";
 
 
+const fireArmsData = await fetch('https://winsapi.discloud.app/dev/products/weapons').then(res => res.json());
+const fireArmsTransformedData = fireArmsData.map((item: any) => ({
+    ...item,
+    images: item.images.map((imageCode: string) => `https://wins.company/assets/TesteFrontEnd/weapons/${imageCode}.webp`)
+}));
 
 export function HeroSection() {
+
+    console.log(fireArmsTransformedData);
     return (
         <div className="relative w-full h-[1030px]  text-white bg-black">
             <img
@@ -16,21 +23,28 @@ export function HeroSection() {
                         EXPLORE AS <br /> SKINS ESPECIAIS
                     </h1>
                     <p className="mt-4 text-lg">
-                        Veja todos os produtos que você pode adquirir com <br/> Quantions em nosso jogo
+                        Veja todos os produtos que você pode adquirir com <br /> Quantions em nosso jogo
                     </p>
                 </div>
                 <img
                     src="/ArmasSoltasDivisorio.svg"
                     alt="Divisor"
-                    
+
                 />
 
-               
+
             </div>
-            <div className= "absolute w-full justify-center items-center h-10 bg-red">
-                <FireArmsCard />
+            <div className="flex gap-2  mt-20 absolute w-full justify-center items-center h-10 bg-red">
+                {fireArmsTransformedData.slice(0, 5).map((item: { name: string; price: string | number; images: string[] }, index: number) => (
+                    <FireArmsCard
+                        key={index}
+                        name={item.name}
+                        price={item.price}
+                        images={item.images[0]}
+                    />
+                ))}
             </div>
-           
+
         </div>
     )
 }
